659887a88d2c4e83850bbbcd111a8e7f670d77ac
durongchun <durongchun@hotmail.com>
2025-03-19 15:44:18 -0700

    adjust detail

--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import "../styles/components/movieDetail.scss";
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
@@ -35,22 +34,45 @@ const MovieDetails = () => {
       });
   }
 
-  if(loading) return <p>Loading...</p>;
-  if(error) return <p>{error}</p>;
-  if(!movie) return <p>No movie found</p>;
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+  if (!movie) return <p>No movie found</p>;
 
-  return(
-    <main className="movie-details">
-        <h2>{movie.title}</h2>
-        <img
+  const backgroundImage = `url(https://image.tmdb.org/t/p/w500${movie.poster_path})`;
+
+  return (
+    <main
+      className="movie-details"
+      style={{
+        backgroundImage: backgroundImage,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        color: "white",
+        padding: "20px",
+      }}
+    >
+      <img
         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         alt={movie.title}
       />
-      <p><strong>Release Date:</strong><br/> {movie.release_date}</p>
-      <p><strong>Rating:</strong><br /> {movie.vote_average}</p>
-      <p><strong>Overview:</strong><br/> {movie.overview}</p>
-      <button onClick={() => window.history.back()}>Go Back</button>
+      <div>
+        <h2>{movie.title}</h2>
+
+        <p>
+          <strong>Release Date:</strong>
+          <br /> {movie.release_date}
+        </p>
+        <p>
+          <strong>Rating:</strong>
+          <br /> {movie.vote_average}
+        </p>
+        <p>
+          <strong>Overview:</strong>
+          <br /> {movie.overview}
+        </p>
+        <button onClick={() => window.history.back()}>Go Back</button>
+      </div>
     </main>
-  )
+  );
 };
 export default MovieDetails;
