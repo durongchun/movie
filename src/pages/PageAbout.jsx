f4c783c4aafa766b3646d5beea41d2cefb39db93
durongchun <durongchun@hotmail.com>
2025-03-19 19:11:34 -0700

    change about

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -3,28 +3,30 @@ import React from "react";
 function PageAbout() {
   return (
     <main className="about-page">
-      <div className="wrapper">
-        <h2>About Our Movie Website</h2>
-        <div className="about-content">
-          <p>
+      <h2>Welcome CINESTREAM</h2>
+      <div className="about-content">
+        <div>
+          <h4>
             Welcome to our premier movie discovery platform! We are passionate
             about bringing the magic of cinema right to your fingertips. Our
             website offers a comprehensive collection of movies from various
             genres, eras, and cultures, making it easier than ever to explore
             and discover your next favorite film.
-          </p>
-          <p>
+          </h4>
+          <h4>
             With our intuitive search feature, you can quickly find movies by
             title, genre, or cast members. Want to save movies for later? Our
             "Watch Later" feature allows you to create your personal watchlist,
             ensuring you never miss out on films you're interested in.
-          </p>
-          <p>
+          </h4>
+        </div>
+        <div>
+          <h4>
             We pride ourselves on providing detailed information about each
             movie, including ratings, reviews, cast details, and plot summaries.
             Whether you're a casual viewer or a dedicated cinephile, our
             platform is designed to enhance your movie-watching experience.
-          </p>
+          </h4>
         </div>
       </div>
     </main>
