5fa1053bb53970aea3baabf80091b9e5bd1168bc
durongchun <durongchun@hotmail.com>
2025-03-20 07:55:58 -0700

    change footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
       <img src="../../public/movielogo-removebg-preview.png" alt="MVDBlogo" />
       <div>
         <p>&copy; {new Date().getFullYear()} MVDB all rights reserved.</p>
-        <p>Created by Zahra Fatemi, Kyungmi Moon, Kay Watanabe</p>
+        <p>Created by Goli Fatemi, Caroline, Kkaur, Lucy Du</p>
       </div>
     </footer>
   );
